5d0c2e44ffe5ebd3e903d9e3d601bbc53fe2ae64
EduardoProfe <eduardoglez64377@gmail.com>
2024-01-04 15:29:08 -0500

    mail

--- router/paginas.py
+++ router/paginas.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
+from fastapi_mail import ConnectionConfig, MessageSchema, MessageType, FastMail
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 
@@ -60,4 +61,44 @@ async def root(request: Request):
     return templates.TemplateResponse("p-sec-1.html", {
         "request": request,
         "titulo": "Estadísticas"
+    })
+
+
+@router.post("/email", response_class=HTMLResponse)
+async def root(request: Request, name: str = Form(...), email: str = Form(...), message: str = Form(...)):
+    conf = ConnectionConfig(
+        MAIL_USERNAME="fastapicalculadora@gmail.com",
+        MAIL_PASSWORD="cxnnzmjekaqbsuyd",
+        MAIL_FROM="fastapicalculadora@gmail.com",
+        MAIL_PORT=587,
+        MAIL_SERVER="smtp.gmail.com",
+        MAIL_FROM_NAME="FastApi Calculadora Simple",
+        MAIL_STARTTLS=True,
+        MAIL_SSL_TLS=False,
+        USE_CREDENTIALS=True,
+        VALIDATE_CERTS=True
+    )
+
+
+    html = f"""
+           <h1>FastApi Calculadora Simple</h1>
+           <h3>Nombre: {name}</h3>
+           <h3>Correo: {email}</h3>
+           <h3>Mensaje:</h3>
+           <p>{message}</p>
+       """
+
+    message = MessageSchema(
+        subject="Formulario de FastApi Calculadora Simple",
+        recipients=["fastapicalculadora@gmail.com"],
+        body=html,
+        subtype=MessageType.html
+    )
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+
+    return templates.TemplateResponse("p-sec-1.html", {
+        "request": request,
+        "titulo": "Email Enviado"
     })
