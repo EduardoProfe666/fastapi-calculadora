390056c8d396f6a23f67fe2316f885c16ea6dfe6
EduardoProfe <eduardoglez64377@gmail.com>
2024-01-04 13:27:58 -0500

    no-job

--- jobs/render-update.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://fastapi-calculadora.onrender.com/')
- .then((response) => {
-   console.log(response.data);
- })
- .catch((error) => {
-   console.error(error);
- });
